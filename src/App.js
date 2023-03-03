841e2b8db3f83d220fda534a14603ebb17bf2a0d
emmanuelguty <manuel15901@gmail.com>
2023-03-02 18:49:15 -0600

    Inicio de proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+
+import Home from '../src/pages/Home/Home'
+import Login from '../src/pages/Login/Login'
+import Register from '../src/pages/Register/Register'
+import Booking from '../src/pages/BookingCar/BookingCar'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/bookingcar" element={<Booking />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+        <h1>Home Page</h1>
+    </div>
+  )
+}
+
+export default Register
